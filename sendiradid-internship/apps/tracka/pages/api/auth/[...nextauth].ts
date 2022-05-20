681c45b5a37dd2a050b154eaa5170c963289ba28
Optic-Dex <danielthomsen238@gmail.com>
2022-05-20 13:25:18 +0000

    next auth login

---
+++ sendiradid-internship/apps/tracka/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,119 @@
+import NextAuth, { Session } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { ILoginResponse } from '../../../src/lib/types'
+import axios from "axios";
+
+
+
+const BASE_URL_APP = "https://app.clickup.com/v1";
+
+interface Credentials {
+    username: string;
+    password: string;
+  }
+
+type Maybe = string|unknown
+
+interface SessionUser {
+  accessToken: Maybe
+  refreshToken: Maybe
+  oAuthToken:Maybe
+  id: Maybe
+  defaultTeam: Maybe
+}
+  
+  
+  export const login = (credentials: Credentials): Promise<ILoginResponse> => {
+    // validate credentials
+    const { username, password } = credentials;
+    if (!username || !password) {
+      throw new Error('Invalid credentials');
+    }
+  
+    // create basic auth header
+    const auth = Buffer.from(`${username}:${password}`).toString('base64');
+    const headers = {
+      Authorization: `Basic ${auth}`,
+    };
+  
+    return new Promise((res, rej) => {
+      axios
+        .post<ILoginResponse>(`${BASE_URL_APP}/login?include_teams=true`, null, {
+          headers,
+        })
+        .then((response) => {
+          const { data } = response;
+          res(data);
+        })
+        .catch((error) => {
+          console.log(error);
+          rej(error);
+        });
+    });
+  };
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Tracka",
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        console.log(credentials);
+
+        try {
+          const { user, token, refresh_token } = await login(credentials);
+          console.log(user)
+          return { ...user, token, refresh_token };
+        } catch (error) {
+          console.log(error)
+          return null;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      console.log("im here")
+      return baseUrl;
+    },
+    async session({ session, token }) {
+      console.log("im here 2")
+
+      const newSession: SessionUser = {
+        accessToken: token.accessToken,
+        refreshToken: token.refreshToken,
+        oAuthToken: token.oAuthToken,
+        id: token.id,
+        defaultTeam: token.defaultTeam
+
+      }
+      session.user = newSession as any
+      return session;
+    },
+    async jwt({ token, user, account }) {
+      if (account && user) {
+        
+        return {
+          ...token,
+          accessToken: user.token,
+          refreshToken: user.refresh_token,
+          oAuthToken: user.oAuthToken,
+          name: user.username,
+          id: user.id,
+          defaultTeam: user.default_team,
+        } as any
+      }
+   
+      return token;
+    },
+  },
+});
