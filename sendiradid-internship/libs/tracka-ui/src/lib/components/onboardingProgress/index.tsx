907acbb509154abf15415f6d8b068ab25037b0d4
Andri Andresson <andriandresson@gmail.com>
2022-06-21 13:38:25 +0000

    feat: updated onboardingStepper

--- sendiradid-internship/libs/tracka-ui/src/lib/components/onboardingProgress/index.tsx
+++ sendiradid-internship/libs/tracka-ui/src/lib/components/onboardingProgress/index.tsx
@@ -1,53 +1,32 @@
-import { useState } from 'react';
+import { FC } from 'react';
 import Box from '@mui/material/Box';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
-import StepContent from '@mui/material/StepContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/material';
 
-const steps = [
-  {
-    label: 'Select team',
-    description: ['sendiradid'],
-  },
-  {
-    label: 'Select space',
-    description:
-      'An ad group contains one or more ads which target a shared set of keywords.',
-  },
-  {
-    label: 'Map customer type',
-    description: `Try out different ad text to see what brings in the most customers,
-              and learn how to enhance your ads using features like ad extensions.
-              If you run into any problems with your ads, find out how to tell if
-              they're running and how to resolve approval issues.`,
-  },
-  {
-    label: 'Review',
-    description: `Try out different ad text to see what brings in the most customers,
-              and learn how to enhance your ads using features like ad extensions.
-              If you run into any problems with your ads, find out how to tell if
-              they're running and how to resolve approval issues.`,
-  },
-];
 
-// interface Props {
-//     label: string;
-//     description: string;
-// }
+type Selection = string | string[] | undefined;
 
-export const OnboardingProgress = () => {
-  const [activeStep, setActiveStep] = useState(0);
- 
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
+interface Step {
+  label: string;
+  selected?: Selection;
+}
 
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
+interface Props {
+  steps: Step[]
+  activeStep: number
+}
+
+export const OnboardingProgress: FC<Props> = ({ steps, activeStep }) => {
+  const { palette } = useTheme()
+  const getSelectionText = (selected: Selection) => {
+    if (Array.isArray(selected)) {
+      return selected.length > 1 ? `${selected.length} spaces selected` : selected[0];
+    }
+    return selected;
+  }
 
 
   return (
@@ -55,25 +34,13 @@ export const OnboardingProgress = () => {
       <Stepper activeStep={activeStep} orientation="vertical">
         {steps.map((step, index) => (
           <Step key={step.label}>
-            <StepLabel>{step.label}</StepLabel>
-            <StepContent sx={{ display: 'flex', flexDirection: 'column' }}>
-              <Typography sx={{ color: '#fff' }}>{step.description}</Typography>
-              <Box>
-                <div>
-                  <Button onClick={handleNext}>
-                    {index === steps.length - 1 ? 'Finish' : 'Next'}
-                  </Button>
-
-                  <Button onClick={handleBack}>Back</Button>
-                </div>
-              </Box>
-            </StepContent>
+            <StepLabel>
+              <Typography sx={{ color: '#fff' }}>{step.label}</Typography>
+              {step.selected && <Typography sx={{ color: palette.grey[700] }}>{getSelectionText(step.selected)}</Typography>}
+            </StepLabel>
           </Step>
         ))}
       </Stepper>
-      {activeStep === steps.length && (
-        <Typography sx={{ color: '#fff' }}>All steps are completed.</Typography>
-      )}
     </Box>
   );
 };
