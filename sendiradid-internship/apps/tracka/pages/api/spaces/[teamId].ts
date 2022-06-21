bf8b79f42f30ca7c0f2372668e8e74ac7d7df81a
Andri Andresson <andriandresson@gmail.com>
2022-06-21 14:52:45 +0000

    feat: updated handler structure

--- sendiradid-internship/apps/tracka/pages/api/spaces/[teamId].ts
+++ sendiradid-internship/apps/tracka/pages/api/spaces/[teamId].ts
@@ -1,7 +1,10 @@
 import { getSession } from 'next-auth/react';
 import axios from 'axios';
 
+// Fetching all spaces for a team
+
 const Handler = async (req, res) => {
+
   const session = await getSession({ req });
 
   if (!session || !session.user) {
@@ -9,19 +12,29 @@ const Handler = async (req, res) => {
   }
 
   const accessToken = session.user['accessToken'];
-  const teamID = await session.user['defaultTeam'];
-  console.log(teamID);
+
+  const { teamId = null } = req.query;
+  if (!teamId) {
+    return res.status(400).json({ message: 'no team id' });
+  }
+
   const config = {
     headers: {
       Authorization: accessToken,
     },
   };
-  const respons = await axios.get(
-    `https://api.clickup.com/api/v2/team/${teamID}/space`,
-    config
-  );
-  const { data } = respons;
-  res.status(200).json(data);
+  try {
+    const respons = await axios.get(
+      `https://api.clickup.com/api/v2/team/${teamId}/space`,
+      config
+    );
+    const { data } = respons;
+    res.status(200).json(data);
+  }
+  catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'error' });
+  }
 };
 
 export default Handler;
