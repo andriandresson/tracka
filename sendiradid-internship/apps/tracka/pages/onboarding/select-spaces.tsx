3694e17545654336ec192135a2367490ce75d040
Sebastian <sebastianlenart@gmail.com>
2022-06-23 14:09:20 +0000

    select spaces onboarding page
    created boilerplate for select spaces onboarding page based on chosen team, adding missing api route for fetching spaces from ClickUpAPI

--- sendiradid-internship/apps/tracka/pages/onboarding/select-spaces.tsx
+++ sendiradid-internship/apps/tracka/pages/onboarding/select-spaces.tsx
@@ -1,29 +1,113 @@
-import { withAuth } from "apps/tracka/components/withAuth";
-import axios from "axios";
-import { useQuery } from "react-query";
+import { useApplicationContext } from '../../components/appContext';
+import axios from 'axios';
+import { useQuery } from 'react-query';
+import {
+  Selection,
+  OnboardingLayout,
+  LabelBox,
+} from '@sendiradid-internship/tracka-ui';
+import { Container, Button } from '@mui/material';
+import Link from 'next/link';
 
-
-const fetchSpaces = async (teamId: string) => {
-  const { data } = await axios.get(`api/spaces/${teamId}`);
-  return data;
-}
+const fetchSpaces = async (teamId: string | number) => {
+  if (typeof teamId === 'string') {
+    const { data } = await axios.get(`/api/spaces/${teamId}`);
+    console.log(data);
+    return data;
+  } else {
+    const { data } = await axios.get(`/api/spaces/${teamId.toString()}`);
+    console.log(data);
+    return data;
+  }
+};
 
 const SelectSpaces = ({ session }) => {
   const { user } = session;
+  const { value, setValue } = useApplicationContext();
+  // setValue('activeStep', 1);
+  console.clear();
+  console.log('context: ', value);
+  console.log('team', value.steps[0].selected);
 
-  // Todo get selected team from context
-  const defaultTeam = user["defaultTeam"];
+  const selectedTeam = () => {
+    // check if selected is array
+    if (Array.isArray(value.steps[0].selected)) {
+      const teams = value.steps[0].selected as Selection[];
+      return teams[0]?.id;
+    }
+    const stepTeam = value.steps[0].selected as Selection;
+    return stepTeam?.id;
+  };
 
-  const { data, isLoading, isError } = useQuery("spaces", () => fetchSpaces(defaultTeam));
+  const teamId = selectedTeam();
+
+  console.log('teamId:', teamId);
+  const { data, isLoading, isError } = useQuery('spaces', () =>
+    fetchSpaces(teamId)
+  );
+  console.log('data:', data);
+  if (isLoading) {
+    return <Container>loading...</Container>;
+  }
+
+  if (isError) {
+    return <Container>Error</Container>;
+  }
 
   return (
-    <div style={{ color: '#fff' }}>
-      {isLoading ? <div>Loading...</div> : null}
-      {isError ? <div>Error</div> : null}
-      {data && <h1>We got Data</h1>}
-    </div>
+    <OnboardingLayout
+      title={value.steps[value.activeStep].label}
+      description={value.steps[value.activeStep].description}
+      activeStep={value.activeStep}
+      steps={value.steps}
+    >
+      <Container
+        sx={{
+          mt: 8,
+          alignSelf: 'flex-start',
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr 1fr',
+          gridGap: 80,
+          justifyItems: 'start',
+          alignItems: 'start',
+          justifyContent: 'start',
+        }}
+      >
+        {data.spaces.map((space) => (
+          <Container key={space.id} onClick={() => console.log(space)}>
+            <LabelBox
+              name={space.name}
+              color={space.color}
+              avatar={space.avatar}
+              active={false}
+            />
+          </Container>
+        ))}
+      </Container>
+      <Link href="/onboarding/select-team">
+        <Button
+          variant="outlined"
+          sx={{
+            mt: 8,
+            width: 200,
+            alignSelf: 'flex-end',
+          }}
+          onClick={() => setValue('activeStep', value.activeStep - 1)}
+        >
+          Back
+        </Button>
+      </Link>
+      <Link href="/onboarding/select-spaces">
+        <Button
+          variant="contained"
+          sx={{ mt: 8, width: 200, alignSelf: 'flex-end' }}
+          onClick={() => setValue('activeStep', value.activeStep + 1)}
+        >
+          Continue
+        </Button>
+      </Link>
+    </OnboardingLayout>
   );
 };
 
-
-export default SelectSpaces
+export default SelectSpaces;
