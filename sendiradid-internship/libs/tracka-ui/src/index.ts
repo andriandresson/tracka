3320cf7a076911e09f1447f5feb03f2156ba6005
Sebastian <sebastianlenart@gmail.com>
2022-07-19 15:20:11 +0000

    old component export deleted

--- sendiradid-internship/libs/tracka-ui/src/index.ts
+++ sendiradid-internship/libs/tracka-ui/src/index.ts
@@ -6,6 +6,5 @@ export * from './lib/types';
 export * from './lib/components/onboardingLayout';
 export * from './lib/components/onboardingCustomerSelect';
 export * from './lib/components/accordion';
-export * from './lib/components/employeeTimeTracker';
 export * from './lib/components/employeeTimeTrackerV2';
 export * from './lib/components/customDateRangePicker';
