4b6d832bd25da3d4a181947129d393ddf43dc1ad
JakubM1999 <89194895+JakubM1999@users.noreply.github.com>
2022-07-26 11:33:45 +0000

    Styling time tracker widget UPDATE
    Added Skeleton component

--- sendiradid-internship/libs/tracka-ui/src/lib/components/employeeTimeTrackerV2/index.tsx
+++ sendiradid-internship/libs/tracka-ui/src/lib/components/employeeTimeTrackerV2/index.tsx
@@ -183,7 +183,66 @@ export const EmployeeTimeTrackerWidgetV2: FC<Props> = ({ teamID }) => {
           bgcolor: 'background.paper'
         }}
       >
-        loading...
+        <Box sx={{display:'flex', pt:3, gap: 30}}>
+        <Skeleton sx={{flexGrow:1, width:100, height:40}}/>
+        <Skeleton sx={{flexGrow:1, width:100, height:20}}/> 
+        </Box>
+        <Box sx={{display:'flex', pt:3, gap:40}}> 
+        <Skeleton sx={{flexGrow:1, width:60, height:20}}/>
+        <Skeleton sx={{flexGrow:1, width:60, height:20}}/> 
+        </Box>
+        <Box sx={{display:'flex', flexDirection:'column', gap:2, alignContent:'flex-start', pt:1.5}}>
+          <Box sx={{display:'flex', flexDirection:'row', gap:2}}>
+            <Skeleton variant='circular' sx={{width:40, height:40}}/>
+            <Box sx={{display:'flex', flexDirection:'row', gap:23}}>
+            <Box sx={{display:'flex', flexDirection:'column'}}> 
+            <Skeleton variant='text' sx={{width:140, height:25}}/>
+            <Skeleton variant='text' sx={{width:70, height:15}}/>
+            </Box>
+            <Skeleton variant='text' sx={{width:55, height:25}}/>
+            </Box>
+          </Box>
+          <Box sx={{display:'flex', flexDirection:'row', gap:2}}>
+            <Skeleton variant='circular' sx={{width:40, height:40}}/>
+            <Box sx={{display:'flex', flexDirection:'row', gap:23}}>
+            <Box sx={{display:'flex', flexDirection:'column'}}> 
+            <Skeleton variant='text' sx={{width:140, height:25}}/>
+            <Skeleton variant='text' sx={{width:70, height:15}}/>
+            </Box>
+            <Skeleton variant='text' sx={{width:55, height:25}}/>
+            </Box>
+          </Box>
+          <Box sx={{display:'flex', flexDirection:'row', gap:2}}>
+            <Skeleton variant='circular' sx={{width:40, height:40}}/>
+            <Box sx={{display:'flex', flexDirection:'row', gap:23}}>
+            <Box sx={{display:'flex', flexDirection:'column'}}> 
+            <Skeleton variant='text' sx={{width:140, height:25}}/>
+            <Skeleton variant='text' sx={{width:70, height:15}}/>
+            </Box>
+            <Skeleton variant='text' sx={{width:55, height:25}}/>
+            </Box>
+          </Box>
+          <Box sx={{display:'flex', flexDirection:'row', gap:2}}>
+            <Skeleton variant='circular' sx={{width:40, height:40}}/>
+            <Box sx={{display:'flex', flexDirection:'row', gap:23}}>
+            <Box sx={{display:'flex', flexDirection:'column'}}> 
+            <Skeleton variant='text' sx={{width:140, height:25}}/>
+            <Skeleton variant='text' sx={{width:70, height:15}}/>
+            </Box>
+            <Skeleton variant='text' sx={{width:55, height:25}}/>
+            </Box>
+          </Box>
+          <Box sx={{display:'flex', flexDirection:'row', gap:2}}>
+            <Skeleton variant='circular' sx={{width:40, height:40}}/>
+            <Box sx={{display:'flex', flexDirection:'row', gap:23}}>
+            <Box sx={{display:'flex', flexDirection:'column'}}> 
+            <Skeleton variant='text' sx={{width:140, height:25}}/>
+            <Skeleton variant='text' sx={{width:70, height:15}}/>
+            </Box>
+            <Skeleton variant='text' sx={{width:55, height:25}}/>
+            </Box>
+          </Box>
+        </Box>
       </Container>
     );
   }
